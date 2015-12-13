a41cc579657090baf0829675a5c26b7f4beb4d89
Dianwen Li <dianwen@gmail.com>
2015-12-13 13:16:14 -0500

    Added coordinate resolve endpoint

--- plane_weather.py
+++ plane_weather.py
@@ -3,9 +3,21 @@ import logging
 import sys
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.DEBUG)
+
 
 @app.route('/')
 def test():
     return 'Hello World!'
+
+
+@app.route('/resolve/<location>')
+def resolve_location(location):
+    if ',' in location:
+        lat_long = location.split(',')
+        lat = lat_long[0]
+        long = lat_long[1]
+
+    return jsonify(location=[lat, long])
